1f972a033daa667cfa374f1073d7e89a1b5b5484
zngrn <vaibhavjhingran@gmail.com>
2022-02-13 12:52:50 +0100

    Add bot greets function to capabilities

--- t2s.py
+++ t2s.py
@@ -1,5 +1,6 @@
 import pyttsx3
 from decouple import config
+from datetime import datetime
 
 USER = config('USER')
 BOT = config('BOT')
@@ -16,6 +17,18 @@ def bot_speak(content):
     engine.runAndWait()
     engine.stop()
 
+def bot_greet():
+    current_hour = datetime.now().hour
+    
+    if current_hour in range(6,12):
+        bot_speak(f"Good Morning! {USER}")
+    elif current_hour in range(12, 16):
+        bot_speak(f"Good afternoon! {USER}")
+    elif current_hour in range(16, 21):
+        bot_speak(f"Good evening! {USER}")
+    bot_speak(f"I'm {BOT}. How can I be of assistance?")
+
 
 if __name__ == "__main__":
     bot_speak('Hello! I am Cosmo!')
+    bot_greet()
